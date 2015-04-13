16a528115f55c6416a1e5735a5d1a89d3e0b5b7e
Chao Meng <mengchao.agile@gmail.com>
2015-04-13 22:02:25 +0800

    Skeleton code for assignment 3 (Programming Mobile Services for Android Handheld Systems: Concurrency)

---
+++ AdvancedDownloadedImageViewer/app/src/main/java/vandy/mooc/DownloadImageActivity.java
@@ -0,0 +1,76 @@
+package vandy.mooc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+/**
+ * An Activity that downloads an image, stores it in a local file on
+ * the local device, and returns a Uri to the image file.
+ */
+public class DownloadImageActivity extends Activity {
+    /**
+     * Debugging tag used by the Android logger.
+     */
+    private final String TAG = getClass().getSimpleName();
+
+    /**
+     * Hook method called when a new instance of Activity is created.
+     * One time initialization code goes here, e.g., UI layout and
+     * some class scope variable initialization.
+     *
+     * @param savedInstanceState object that contains saved state information.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Always call super class for necessary
+        // initialization/implementation.
+        super.onCreate(savedInstanceState);
+
+        // Get the URL associated with the Intent data.
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        // Download the image in the background, create an Intent that
+        // contains the path to the image file, and set this as the
+        // result of the Activity.
+
+        // fill in here using the Android "HaMeR"
+        // concurrency framework.  Note that the finish() method
+        // should be called in the UI thread, whereas the other
+        // methods should be called in the background thread.  See
+        // http://stackoverflow.com/questions/20412871/is-it-safe-to-finish-an-android-activity-from-a-background-thread
+        // for more discussion about this topic.
+        DownloaderTask downloaderTask = new DownloaderTask();
+
+        // Start the DownloaderTask
+        downloaderTask.execute(uri);
+
+    }
+
+    // Implement an AsyncTask subclass called DownLoaderTask.
+    // This class must use the downloadTweets method (currently commented
+    // out). Ultimately, it must also pass newly available data back to
+    // the hosting Activity using the DownloadFinishedListener interface.
+
+    public class DownloaderTask extends AsyncTask<Uri, Void, Uri> {
+
+        @Override
+        protected Uri doInBackground(Uri... url) {
+            return Utils.downloadImage(getApplicationContext(), url[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Uri url) {
+            Intent data = new Intent();
+            data.putExtra("data", url.toString());
+
+            setResult(Activity.RESULT_OK, data);
+            finish();
+        }
+
+    }
+
+}
---
+++ AdvancedDownloadedImageViewer/app/src/main/java/vandy/mooc/MainActivity.java
@@ -0,0 +1,200 @@
+package vandy.mooc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.URLUtil;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * A main Activity that prompts the user for a URL to an image and
+ * then uses Intents and other Activities to download the image and
+ * view it.
+ */
+public class MainActivity extends LifecycleLoggingActivity {
+    /**
+     * Debugging tag used by the Android logger.
+     */
+    private final String TAG = getClass().getSimpleName();
+
+    /**
+     * A value that uniquely identifies the request to download an
+     * image.
+     */
+    private static final int DOWNLOAD_IMAGE_REQUEST = 1;
+
+    /**
+     * EditText field for entering the desired URL to an image.
+     */
+    private EditText mUrlEditText;
+
+    /**
+     * URL for the image that's downloaded by default if the user
+     * doesn't specify otherwise.
+     */
+    private Uri mDefaultUrl =
+        Uri.parse("http://www.dre.vanderbilt.edu/~schmidt/robot.png");
+
+    /**
+     * Hook method called when a new instance of Activity is created.
+     * One time initialization code goes here, e.g., UI layout and
+     * some class scope variable initialization.
+     *
+     * @param savedInstanceState object that contains saved state information.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Always call super class for necessary
+        // initialization/implementation.
+        super.onCreate(savedInstanceState);
+
+        // Set the default layout.
+        setContentView(R.layout.main_activity);
+
+        // Cache the EditText that holds the urls entered by the user
+        // (if any).
+        mUrlEditText = (EditText) findViewById(R.id.url);
+
+    }
+
+    /**
+     * Called by the Android Activity framework when the user clicks
+     * the "Find Address" button.
+     *
+     * @param view The view.
+     */
+    public void downloadImage(View view) {
+        try {
+            // Hide the keyboard.
+            hideKeyboard(this,
+                         mUrlEditText.getWindowToken());
+
+            // Call the makeDownloadImageIntent() factory method to
+            // create a new Intent to an Activity that can download an
+            // image from the URL given by the user.  In this case
+            // it's an Intent that's implemented by the
+            // DownloadImageActivity.
+            Uri url = getUrl();
+            if (url == null) {
+                return;
+            }
+
+            Intent downloadImageIntent = makeDownloadImageIntent(url);
+
+            // Start the Activity associated with the Intent, which
+            // will download the image and then return the Uri for the
+            // downloaded image file via the onActivityResult() hook
+            // method.
+            startActivityForResult(downloadImageIntent, DOWNLOAD_IMAGE_REQUEST);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Hook method called back by the Android Activity framework when
+     * an Activity that's been launched exits, giving the requestCode
+     * it was started with, the resultCode it returned, and any
+     * additional data from it.
+     */
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        // Check if the started Activity completed successfully.
+        if (resultCode == RESULT_OK) {
+            // Check if the request code is what we're expecting.
+            if (requestCode == DOWNLOAD_IMAGE_REQUEST) {
+                // Call the makeGalleryIntent() factory method to
+                // create an Intent that will launch the "Gallery" app
+                // by passing in the path to the downloaded image
+                // file.
+                Bundle extras = data.getExtras();
+                String imagePath = (String) extras.get("data");
+                Intent galleryIntent = makeGalleryIntent(imagePath);
+
+                // Start the Gallery Activity.
+                startActivity(galleryIntent);
+            }
+        }
+        // Check if the started Activity did not complete successfully
+        // and inform the user a problem occurred when trying to
+        // download contents at the given URL.
+        else {
+            Toast.makeText(this,
+                           "A problem occurred when trying to download contents at the given URL",
+                           Toast.LENGTH_SHORT).show();
+        }
+    }    
+
+    /**
+     * Factory method that returns an implicit Intent for viewing the
+     * downloaded image in the Gallery app.
+     */
+    private Intent makeGalleryIntent(String pathToImageFile) {
+        // Create an intent that will start the Gallery app to view
+        // the image.
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(pathToImageFile)), "image/*");
+        return intent;
+    }
+
+    /**
+     * Factory method that returns an implicit Intent for downloading
+     * an image.
+     */
+    private Intent makeDownloadImageIntent(Uri url) {
+        // Create an intent that will download the image from the web.
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.setData(url);
+        return intent;
+    }
+
+    /**
+     * Get the URL to download based on user input.
+     */
+    protected Uri getUrl() {
+        Uri url = null;
+
+        // Get the text the user typed in the edit text (if anything).
+        url = Uri.parse(mUrlEditText.getText().toString());
+
+        // If the user didn't provide a URL then use the default.
+        String uri = url.toString();
+        if (uri == null || uri.equals(""))
+            url = mDefaultUrl;
+
+        // Do a sanity check to ensure the URL is valid, popping up a
+        // toast if the URL is invalid.
+        if (URLUtil.isValidUrl(url.toString()))
+            return url;
+        else {
+            Toast.makeText(this,
+                           "Invalid URL",
+                           Toast.LENGTH_SHORT).show();
+            return null;
+        } 
+    }
+
+    /**
+     * This method is used to hide a keyboard after a user has
+     * finished typing the url.
+     */
+    public void hideKeyboard(Activity activity,
+                             IBinder windowToken) {
+        InputMethodManager mgr =
+            (InputMethodManager) activity.getSystemService
+            (Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(windowToken,
+                                    0);
+    }
+}
